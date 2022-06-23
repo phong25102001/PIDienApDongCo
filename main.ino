0ec16df2f2c0e2489f92d6fadec21deb7ce703d1
PhongnsEE2 <phong2537vn@gmail.com>
2022-06-23 15:09:32 +0700

    cai dat tan so PWM 3kHZ

--- main.ino
+++ main.ino
@@ -8,26 +8,30 @@ int mean;
 double Setpoint;
 double Input;
 double Output;
-double Kp = 0.8, Ki = 2, Kd = 0;
+double Kp = 0.8, Ki = 2.5, Kd = 0;
 
 PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
 
 void setup()
 {
-  Serial.begin(9600);
-  pinMode(9, OUTPUT);
+  Serial.begin(19200);
   myPID.SetMode(AUTOMATIC);
   myPID.SetTunings(Kp, Ki, Kd);
+  myPID.SetOutputLimits(0,1023);
   
-  Setpoint = map(analogRead(A0),0,1023,0,255);
-  analogWrite(9,Setpoint);
-  delay(5000);
- // myPID.SetSampleTime(1);
+  TCCR1B = TCCR1B & B11111000 | B00000010;  // Set tan so xung PWM 3kHz
+  pinMode(9, OUTPUT);
+  
+  
+  //Setpoint = map(analogRead(A0),0,1023,0,255);
+  //analogWrite(9,Setpoint);
+  //delay(5000);
+  //myPID.SetSampleTime(0.07);
 }
 
 void loop()
 {
-  Setpoint = map(analogRead(A0),0,1023,0,255);
+  Setpoint = analogRead(A0);
   
   long total = 0L;
   
@@ -45,13 +49,12 @@ void loop()
     total = total + tempVoltage[i];
 
   if (count>0)
-    mean = total/count; // mean gan bang Vo
+    mean = int(float(total)/count); // mean gan bang Vo
   count++;
   
-  Input = map(mean,0,1023,0,255); //doc dien ap vao scale de phu hop voi tin hieu ra
+  Input = double(mean); //doc dien ap vao scale de phu hop voi tin hieu ra
   myPID.Compute();
-  analogWrite(9,Output);          // ghi tin hieu dieu khien PWM ra chan so 9
-
+  analogWrite(9,Output/4);          // ghi tin hieu dieu khien PWM ra chan so 9
   Serial.print(Setpoint);
   Serial.print(" ");
   Serial.println(Input);
