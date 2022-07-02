d5cc2b47fcf8034ebebe0e4b5c5126eede1b4c4c
PhongnsEE2 <phong2537vn@gmail.com>
2022-07-02 19:51:41 +0700

    mach phan hoi toc do va dong dien

--- main.ino
+++ main.ino
@@ -1,61 +1,84 @@
 #include <PID_v1.h>
 
-int count = 0;
-int tempVoltage[101];
-int mean;
-// tinh trung binh 100 gia tri de uoc luong dien ap Vo
+#define Aphase 2
+#define Bphase 3
+#define Ia A1
+#define PWM 9
 
-double Setpoint;
-double Input;
-double Output;
-double Kp = 0.8, Ki = 2.5, Kd = 0;
+// biến cần để đọc encoder -> tốc độ rpm
+int rpm;
+int encoderCount = 0;
+const int interrupt0 = 0;
 
-PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
+// thời gian đọc và trích mẫu tốc độ
+int timecho = 100;
+unsigned long long thoigian;
+unsigned long long hientai = 0;
+
+// các thông số PID cho bộ PID tốc độ (vòng ngoài)
+double speedSetpoint = 50.0, speedInput, speedOutput;
+double speedKp = 0.8, speedKi = 4, speedKd = 0;
+
+// các thông số PID cho bộ PID dòng điện (vòng trong)
+double& currentSetpoint = speedOutput; 
+double currentInput, currentOutput;
+double currentKp = 0.8, currentKi = 2.5, currentKd = 0;
+
+// 2 bộ PID mạch vòng dòng điện và mạch vòng tốc độ; điều khiển cascade  
+PID currentPID(&currentInput, &currentOutput, &currentSetpoint, currentKp, currentKi, currentKd, DIRECT);
+PID speedPID(&speedInput,&speedOutput,&speedSetpoint, speedKp, speedKi, speedKd, DIRECT);
 
 void setup()
 {
   Serial.begin(19200);
-  myPID.SetMode(AUTOMATIC);
-  myPID.SetTunings(Kp, Ki, Kd);
-  myPID.SetOutputLimits(0,1023);
   
-  TCCR1B = TCCR1B & B11111000 | B00000010;  // Set tan so xung PWM 3kHz
-  pinMode(9, OUTPUT);
+  pinMode(Aphase, INPUT_PULLUP);
+  pinMode(Bphase, INPUT_PULLUP);
+  attachInterrupt(interrupt0, countPulse, RISING);
   
+  // thiết lập bộ PID dòng
+  currentPID.SetMode(AUTOMATIC);
+  currentPID.SetTunings(currentKp, currentKi, currentKd);
   
-  //Setpoint = map(analogRead(A0),0,1023,0,255);
-  //analogWrite(9,Setpoint);
-  //delay(5000);
-  //myPID.SetSampleTime(0.07);
+  // thiết lập bộ PID tốc độ
+  speedPID.SetMode(AUTOMATIC);
+  speedPID.SetTunings(speedKp, speedKi, speedKd);
+  TCCR1B = TCCR1B & B11111000 | B00000010;  // Set tan so xung PWM fsw = 3kHz
+  pinMode(PWM, OUTPUT);
 }
+
+// Hàm ngắt đếm xung cho encoder để đo tốc độ
+void countPulse()
+{
+  if(digitalRead(Bphase) == LOW)
+    encoderCount++;
+  else 
+    encoderCount--;
+}
+
+
 
 void loop()
 {
-  Setpoint = analogRead(A0);
-  
-  long total = 0L;
-  
-  if (count<100)
-    tempVoltage[count] = analogRead(A1);
-  else
-  {
-    for (int i=0; i<count; i++)
-        tempVoltage[i] = tempVoltage[i+1];
-    tempVoltage[count] = analogRead(A1);
-    count--;
+  // đọc Encoder mỗi 100ms
+  thoigian = millis();
+  if (thoigian - hientai > timecho) //millis thay delay
+  {    
+    hientai = thoigian;
+    rpm = int((encoderCount/(300.0*0.1))*60.0);    
+        /*
+         * Đĩa encoder có 300 xung, chúng ta đo được 120 xung/0.1s
+         * vậy lấy 120/(300*0.1) = x vòng/s
+         * ta được: x*60 = số vòng quay / phút (RPM)
+         */
+    encoderCount = 0;
   }
-  
-  for (int i=0; i<count; i++)
-    total = total + tempVoltage[i];
 
-  if (count>0)
-    mean = int(float(total)/count); // mean gan bang Vo
-  count++;
-  
-  Input = double(mean); //doc dien ap vao scale de phu hop voi tin hieu ra
-  myPID.Compute();
-  analogWrite(9,Output/4);          // ghi tin hieu dieu khien PWM ra chan so 9
-  Serial.print(Setpoint);
-  Serial.print(" ");
-  Serial.println(Input);
+  // Tính toán bộ PID 
+  speedInput = rpm;
+  speedPID.Compute();
+  currentInput = analogRead(Ia)/1023.0;
+  currentPID.Compute();
+  Serial.println(currentOutput);
+  analogWrite(PWM, currentOutput);
 }
